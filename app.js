5e9a1df1d3d0357296098f4009ad0b6c4920ca9f
mildred <mildred.bailon@yahoo.com>
2024-05-13 17:16:54 +0100

    2nd commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const containerEl = document.querySelector('containerEl')
+const leftEl = document.querySelector('left')
+const rightEl = document.querySelector('right')
+
+const userEmailEl = document.querySelector('userEmail')
+const errorEmailEl = document.querySelector('errorEmail')
+const emailEl = document.querySelector('email')
+const submitBtnEl = document.querySelector('submit-btn')
+
